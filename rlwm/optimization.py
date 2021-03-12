65772e501b8f7c3ea97364a8b9ae79a6499ea75a
Igor <palmieri.igor@gmail.com>
2021-03-12 11:52:42 -0300

    Fixed Pool one more time 2

--- rlwm/optimization.py
+++ rlwm/optimization.py
@@ -45,7 +45,7 @@ def search_sessions_solution(opt_routine, model_func, opt_bounds, session_list, 
     funct_dict = {}
     work_args = [(model_func, opt_bounds, s, n_reps, models_path) for s in session_list]
     n_jobs = None if n_jobs <= 0 else n_jobs
-    p = Pool(processess=n_jobs)
+    p = Pool(processes=n_jobs)
     res = p.starmap(opt_routine, work_args)
     p.close()
     p.join()
