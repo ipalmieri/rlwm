0da4938f832340342ddc2f682b873b33d3d44be1
Igor <palmieri.igor@gmail.com>
2021-04-12 13:04:33 -0300

    Added rlwmb model

--- rlwm/models.py
+++ rlwm/models.py
@@ -129,7 +129,7 @@ class CollinsRLBest(BaseModel):
         return pi
 
 
-# RLWMi model class
+# Collins RLWMi model class
 class CollinsRLWM(BaseModel):
     '''RL model with additional mechanisms'''  
     def __init__(self, learning_rate, beta, coupled=False):
@@ -181,7 +181,7 @@ class CollinsRLWM(BaseModel):
             delta = rt - self.__Q[st][ac]
         # Perseveration
         lr = self.learning_rate
-        if rt < 1.0: #delta < 0:
+        if delta < 0.:
             lr = lr*(1. - self.pers)
         # Function updates
         self.__Q[st][ac] = self.__Q[st][ac] + lr*delta  
@@ -206,8 +206,9 @@ class CollinsRLWM(BaseModel):
         return pi
 
 
-# RLWM model class version 2
-class CollinsRLWMAlt(BaseModel):
+
+# RLWM model class based on most recent source 
+class CollinsRLWMalt1(BaseModel):
     '''RL model with additional mechanisms'''  
     def __init__(self, learning_rate, beta, K, coupled=False):
         self.alpha_rl = learning_rate
@@ -281,6 +282,83 @@ class CollinsRLWMAlt(BaseModel):
         return pi
 
 
+# RLWM model merge of rlwmi and alt1 
+class CollinsRLWMalt2(BaseModel):
+    '''RL model with additional mechanisms'''  
+    def __init__(self, learning_rate, beta, coupled=False):
+        self.learning_rate = learning_rate
+        self.beta = beta                        # softmax temperature
+        self.eps = 0.0                          # noise ratio
+        self.phi = 0.0                          # forgetting ratio / decay
+        self.pers = 0.0                         # perseveration param
+        self.init = 0.0                         # init bias param
+        self.eta3_wm = 0.0                      # wm weight in policy calculation
+        self.eta6_wm = 0.0                      # wm weight in policy calculation
+        self.coupled = coupled                  # True for RL + WM interacting model
+        self.__stmap = {}                       # Map of stimuli and respective actions
+        self.__known_stimuli = set()            # stimuli already processed for init bias
+        self.__Q = {}     
+        self.__W = {}
+        self.__Q_init = 0.0     
+        self.__W_init = 0.0
+
+    def init_model(self, stimuli, actions):
+        actions = set(actions)
+        stimuli = set(stimuli)
+        self.__stmap = {st: actions for st in stimuli}
+        self.__Q_init = 1./len(actions) # alternative: 0 
+        self.__W_init = 1./len(actions) # alternative: 0 
+        for st in stimuli:
+            self.__Q[st] = {ac: self.__Q_init for ac in actions}
+            self.__W[st] = {ac: self.__W_init for ac in actions}
+
+    def learn_sample(self, stimulus, action, reward, block_size):
+        #print(sample, block_size)
+        st, ac, rt = stimulus, action, reward
+        # Block size dependent parameters
+        eta_wm = self.eta3_wm if block_size == 3 else self.eta6_wm
+        # Forgetting - fix to case with different Q/W
+        if self.phi > 0:
+            for s, actions in self.__stmap.items():
+                for a in actions:
+                    #self.__Q[s][a] = (1.-self.phi)*self.__Q[s][a] + self.phi*self.__Q_init
+                    self.__W[s][a] = (1.-self.phi)*self.__W[s][a] + self.phi*self.__W_init
+        # Initial bias update  
+        if st not in self.__known_stimuli:
+            self.__Q[st][ac] = self.__Q_init + self.init*(1.0 - self.__Q_init)
+            self.__known_stimuli.add(st)
+        # Delta calculation
+        if self.coupled:
+            delta = rt - (eta_wm*self.__W[st][ac] + (1.-eta_wm)*self.__Q[st][ac])
+        else:
+            delta = rt - self.__Q[st][ac]
+        # Perseveration
+        lr = self.learning_rate
+        if rt < 1.0:
+            lr = lr*(1. - self.pers)
+        # Function updates
+        self.__Q[st][ac] = self.__Q[st][ac] + lr*delta  
+        self.__W[st][ac] = rt  
+
+    def get_policy(self, stimulus, block_size=None, test=False):
+        Q_st = self.__Q[stimulus]
+        W_st = self.__W[stimulus]
+        pi_rl = action_softmax(Q_st, self.beta)
+        pi_wm = action_softmax(W_st, self.beta)
+        # Undirected noise
+        n_a = len(pi_rl.keys())
+        pi_rl = {ac: ((1. - self.eps)*p + self.eps/n_a) for ac, p in pi_rl.items()}
+        pi_wm = {ac: ((1. - self.eps)*p + self.eps/n_a) for ac, p in pi_wm.items()}
+        # Final policy - mixed WM and RL
+        if test:
+            return pi_rl
+        pi = {}
+        eta_wm = self.eta3_wm if block_size == 3 else self.eta6_wm
+        for ac in pi_rl.keys():
+            pi[ac] = eta_wm*pi_wm[ac] + (1. - eta_wm)*pi_rl[ac]
+        return pi
+
+
 # Model: Classic RL
 def model_classic(learning_rate, beta):
     model = CollinsRLClassic(learning_rate, beta)
@@ -328,7 +406,7 @@ def model_rlwmi(learning_rate, beta, decay, pers, eps, init, eta3_wm, eta6_wm):
 
 # Model: RLWM alternative version
 def model_rlwma(alpha_rl, alpha_wm, beta, decay, pers, eps, eta_wm, K):
-    model = CollinsRLWMAlt(alpha_rl, beta, K, coupled=False)
+    model = CollinsRLWMalt1(alpha_rl, beta, K, coupled=False)
     model.alpha_rl = alpha_rl
     model.alpha_wm = alpha_wm
     model.beta = beta
@@ -339,6 +417,18 @@ def model_rlwma(alpha_rl, alpha_wm, beta, decay, pers, eps, eta_wm, K):
     model.K = K
     return model
      
+
+# Model: merge of RLWM and alt1
+def model_rlwmb(learning_rate, beta, decay, pers, eps, init, eta3_wm, eta6_wm):
+    model = CollinsRLWMalt2(learning_rate, beta, coupled=True)
+    model.phi = decay
+    model.pers = pers
+    model.eps = eps
+    model.init = init
+    model.eta3_wm = eta3_wm
+    model.eta6_wm = eta6_wm
+    return model
+
 
 # Returns a model expanding parameters into arguments
 def get_model(model_func, params):
--- run_opt.py
+++ run_opt.py
@@ -9,14 +9,14 @@ import rlwm.optimization as optimization
 BASE_PATH = '/srv/black/data/rlwm'
 DATA_PATH = os.path.join(BASE_PATH, 'dados')
 OUTPUT_PATH = os.path.join(BASE_PATH, 'output')
-MODEL_PATH = os.path.join(BASE_PATH, 'models')
-#MODEL_PATH = None
+#MODEL_PATH = os.path.join(BASE_PATH, 'models')
+MODEL_PATH = None
 OPT_REPS = 20
 OPT_EVALMAX = 1000
 
 CASEIDS = [1, 2, 5, 6, 7, 8, 9, 12, 13, 14, 17, 25, 26, 27, 29, 37, 49, 54, 57, 59, 62, 64, 66, 72, 76, 77, 79, 84, 91, 92, 94, 97, 102, 105, 110, 118, 127, 132, 153, 155, 159, 161, 164, 166, 172, 174, 175, 178, 179, 180, 181, 182, 184, 185, 187, 189, 195, 197, 200, 201, 202, 203, 204, 206, 207, 208, 209, 210, 211, 212, 214, 215, 216, 217, 218, 220, 221, 223, 224, 226, 227]
 
-#CASEIDS = CASEIDS[:10]
+CASEIDS = CASEIDS[:10]
 
 
 
@@ -36,7 +36,7 @@ def main():
                      }
 
     bounds_rlwmi = {'learning_rate': (0., 1.),
-                    'beta':          (0., 10),
+                    'beta':          (50., 50.),
                     'decay':         (0., 1.),
                     'pers':          (0., 1.),
                     'eps':           (0., 1.),
@@ -55,14 +55,24 @@ def main():
                     'K':            (6., 6.)
                    }
 
+    bounds_rlwmb = {'learning_rate': (0., 1.),
+                    'beta':          (50., 50.),
+                    'decay':         (0., 1.),
+                    'pers':          (0., 1.),
+                    'eps':           (0., 1.),
+                    'init':          (0., 0.),
+                    'eta3_wm':       (0., 1.),
+                    'eta6_wm':       (0., 1.)
+                   }
 
-    opt_bounds = bounds_rlwma
-    opt_modelfunc = models.model_rlwma
+
+    opt_bounds = bounds_rlwmb
+    opt_modelfunc = models.model_rlwmb
     opt_session_list = session_list
     opt_reps = OPT_REPS
     opt_evalmax = OPT_EVALMAX
-    opt_filename_sp = 'param_sp_rlwma'
-    opt_filename_ho = 'param_ho_rlwma'
+    opt_filename_sp = 'param_sp_rlwmb'
+    opt_filename_ho = 'param_ho_rlwmb'
 
     print(f'Optimizing cases {[s.caseid for s in opt_session_list]}')
 
